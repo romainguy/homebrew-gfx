c1a2b57fafee79d4a660d91a18008f5ffb29eeb4
Romain Guy <romain.guy@gmail.com>
2021-10-27 14:04:42 -0700

    Add formula for ImageMagick with FFTW support

---
+++ imagemagick.rb
@@ -0,0 +1,98 @@
+class Imagemagick < Formula
+    desc "Tools and libraries to manipulate images in many formats"
+    homepage "https://imagemagick.org/index.php"
+    url "https://www.imagemagick.org/download/releases/ImageMagick-7.1.0-11.tar.xz"
+    sha256 "d2658645e4e7bcb02e634a00f47183fe85d23c2cbd7b98731639a542d53a6e06"
+    license "ImageMagick"
+    head "https://github.com/ImageMagick/ImageMagick.git", branch: "main"
+  
+    livecheck do
+      url "https://download.imagemagick.org/ImageMagick/download/"
+      regex(/href=.*?ImageMagick[._-]v?(\d+(?:\.\d+)+-\d+)\.t/i)
+    end
+  
+    bottle do
+      sha256 arm64_monterey: "fbb6e053277c9494260988da6ddd8e264905d2f86d71bc3a8ebfeedbd53a9fb7"
+      sha256 arm64_big_sur:  "f28e89f365bc8f87b623df3bc6e8c5ae3fb35d5fffe30808035482a5071797a7"
+      sha256 monterey:       "5997ee9f869f7a8dd881c7b1487e63670cc315604533ac19e0c49db849fe6db8"
+      sha256 big_sur:        "9b390730a6608fa2fbc5b5553c67ff7ac8b73549d1d7cf6932b0c2aa87244827"
+      sha256 catalina:       "f4b99c905cf22f5573d1b2e7f25d91bd24ccf781edfa9d933d2943c7ae8a72d9"
+      sha256 x86_64_linux:   "9625b08c875f24f1e8dd22559d0428502f7875fed564f8f0f29beb7e349ace55"
+    end
+  
+    depends_on "pkg-config" => :build
+    depends_on "freetype"
+    depends_on "ghostscript"
+    depends_on "jpeg"
+    depends_on "libfftw"
+    depends_on "libheif"
+    depends_on "liblqr"
+    depends_on "libomp"
+    depends_on "libpng"
+    depends_on "libtiff"
+    depends_on "libtool"
+    depends_on "little-cms2"
+    depends_on "openexr"
+    depends_on "openjpeg"
+    depends_on "webp"
+    depends_on "xz"
+  
+    uses_from_macos "bzip2"
+    uses_from_macos "libxml2"
+    uses_from_macos "zlib"
+  
+    on_linux do
+      depends_on "libx11"
+    end
+  
+    skip_clean :la
+  
+    def install
+      # Avoid references to shim
+      inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+  
+      args = [
+        "--enable-osx-universal-binary=no",
+        "--prefix=#{prefix}",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--disable-opencl",
+        "--enable-shared",
+        "--enable-static",
+        "--with-freetype=yes",
+        "--with-gvc=no",
+        "--with-modules",
+        "--with-openjp2",
+        "--with-openexr",
+        "--with-webp=yes",
+        "--with-heic=yes",
+        "--with-gslib",
+        "--with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts",
+        "--with-lqr",
+        "--with-fftw",
+        "--without-pango",
+        "--without-wmf",
+        "--enable-openmp",
+        "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+        "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+        "LDFLAGS=-lomp -lz",
+      ]
+  
+      args << "--without-x" if OS.mac?
+  
+      # versioned stuff in main tree is pointless for us
+      inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+      system "./configure", *args
+      system "make", "install"
+    end
+  
+    test do
+      assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+      # Check support for recommended features and delegates.
+      features = shell_output("#{bin}/convert -version")
+      %w[Modules freetype jpeg png tiff].each do |feature|
+        assert_match feature, features
+      end
+      assert_match "Helvetica", shell_output("#{bin}/identify -list font")
+    end
+  end
